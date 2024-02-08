7e1e7c4523dcafd6585547d5f91905f761b34999
ibragim <ibragim@email.ru>
2024-02-08 21:42:01 +0300

    not complete task2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿int a = 42;
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
+
+int a = 42;
 if (a % 7 == 0 && a % 23 == 0)
 {
     Console.WriteLine("Угадал"); 
@@ -6,4 +9,25 @@ if (a % 7 == 0 && a % 23 == 0)
 else
 {
     Console.WriteLine("Лучше думай. Число не подходит");
+}
+
+ A (x, y);
+ int x = 2;
+ int y = 3;
+
+if ( x > 0 && y > 0)
+{
+    Console.WriteLine("Данная точка в первой плокости");
+} 
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("Данная точка во второй плокости");
+} 
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("Данная точка в третьей плокости");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("Данная точка в четвертой плокости");
 }
